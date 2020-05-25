7e1d494a2301616e06978e4904342f165ee5378b
Ji Park <61026408+jiparkdev@users.noreply.github.com>
2020-05-25 15:56:51 -0700

    Make some progress on App component.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div className="quote-container">
+      <div className="quote-box">
+        <div className="quote-box-top">
+          <p className="quote-text">
+            Code is expensive to change, but design is cheaper to change, and
+            requirements are even cheaper to change.
+          </p>
+          <p className="quote-author">Daniel T. Barry</p>
+        </div>
+        <div className="quote-box-bottom">
+          <img
+            src="icons8-twitter.svg"
+            alt="Share via Twitter"
+            className="quote-twitter"
+          />
+          <button className="quote-button">New Quote</button>
+        </div>
+      </div>
     </div>
   );
 }
